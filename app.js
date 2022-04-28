4cfdeed919a35642d26594ac993e3208a7137caf
Dan <danmanor11@gmail.com>
2022-04-28 16:03:26 +0300

    adding Review model, adding and removing reviews, navbar, footer, stars css, forms validation

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const Landscape = require('./models/landscape')
+const Review = require('./models/review')
 const methodOverride = require('method-override');
+const path = require('path');
 
 mongoose.connect('mongodb://localhost:27017/landscapesDB');
 const db = mongoose.connection;
@@ -14,9 +16,14 @@ db.once("open", () => {
 const app = express();
 
 // middlewear
+
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // routes
 
@@ -29,9 +36,50 @@ app.get('/landscapes', async (req, res) => {
     res.render('landscapes/index', { landscapes })
 })
 
+app.post('/landscapes', async (req, res) => {
+    const landscape = new Landscape(req.body.landscape);
+    await landscape.save();
+    res.redirect(`/landscapes/${landscape._id}`);
+})
+
+app.get('/landscapes/new', (req, res) => {
+    res.render('landscapes/new');
+})
+
 app.get('/landscapes/:id', async (req, res) => {
-    const landscape = await Landscape.findById(req.params.id);
+    const landscape = await Landscape.findById(req.params.id).populate({path: 'reviews'})
     res.render('landscapes/show', { landscape })
+})
+
+app.get('/landscapes/:id/edit', async (req, res) => {
+    const landscape = await Landscape.findById(req.params.id);
+    res.render('landscapes/edit', { landscape });
+})
+
+app.put('/landscapes/:id', async (req, res) => {
+    const landscape = await Landscape.findByIdAndUpdate(req.params.id, { ...req.body.landscape });
+    res.redirect(`/landscapes/${landscape._id}`);
+})
+
+app.delete('/landscapes/:id', async (req, res) => {
+    const landscape = await Landscape.findByIdAndDelete(req.params.id);
+    res.redirect('/landscapes')
+})
+
+app.post('/landscapes/:id/reviews', async (req, res) => {
+    const landscape = await Landscape.findById(req.params.id);
+    const review = new Review(req.body.review);
+    landscape.reviews.push(review);
+    await review.save();
+    await landscape.save();
+    res.redirect(`/landscapes/${landscape._id}`);
+})
+
+app.delete('/landscapes/:id/reviews/:reviewId', async (req, res) => {
+    const landscape = await Landscape.findById(req.params.id);
+    await Landscape.findByIdAndUpdate(req.params.id, { $pull: { reviews: req.params.reviewId } });
+    await Review.findByIdAndDelete(req.params.reviewId);
+    res.redirect(`/landscapes/${landscape._id}`);
 })
 
 // listening
--- models/landscape.js
+++ models/landscape.js
@@ -5,7 +5,23 @@ const LandscapeSchema = new Schema({
     title: String,
     location: String,
     description: String,
-    image: String
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+})
+
+LandscapeSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
 })
 
 module.exports = mongoose.model('Landscape', LandscapeSchema);
